cd9e70443a858e57019ab0a68d28dea0ea5e9280
rohit-specs <rohit5592006@gmail.com>
2025-09-03 15:30:00 +0530

    calculate marks

---
+++ marksheet.py
@@ -0,0 +1,8 @@
+english=int(input("Please enter marks scored in English: "))
+Maths=int(input("Please enter marks scored in Mathematics: "))
+social_science=int(input("Please enter marks scored in Social Science: "))
+science=int(input("Please enter marks scored in Science: "))
+hindi=int(input("Please enter marks scored in Hindi: "))
+Total_marks=english+Maths+social_science+science+hindi
+print("\nTotal marks scored: ",Total_marks)
+print("Percentage obtained: ",Total_marks/5)
